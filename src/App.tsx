5fb30e8ff5a0f8b10162c1f4480d390fe2ea75d4
Uri Elias <urieliasaks@hotmail.com>
2024-02-20 22:36:09 -0600

    Added react typescript vite template

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Route></Route>
+        </BrowserRouter>
+    );
+}
+
+export default App;
